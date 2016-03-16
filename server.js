83a05be6a634fbebb36af14ff7edf25246086e1e
Eddie Tamakloe <tsa99kojo@gmail.com>
2016-03-15 23:47:04 -0400

    Testing git

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var mongojs = require('mongojs');
 var bodyParser = require('body-parser');
 
 
-var db = mongojs('mongodb//Admin:Admin123@ds059365.mlab.com:59365/guru99',[contactlist]);
+var db = mongojs('mongodb//admin:admin123@ds059365.mlab.com:59365/guru99',[contactlist]);
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
